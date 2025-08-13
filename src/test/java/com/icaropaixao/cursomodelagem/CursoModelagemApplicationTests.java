31622d205335ae468741e96f52a536b850ad3287
Icaro Reis <icaroreis1982@gmail.com>
2025-08-13 20:21:25 -0300

    Projeto Criado

---
+++ src/test/java/com/icaropaixao/cursomodelagem/CursoModelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icaropaixao.cursomodelagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
