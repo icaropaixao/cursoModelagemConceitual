5bd5e00f0380861d90bd5f49c6ef7b0aef778e7f
Icaro Reis <icaroreis1982@gmail.com>
2025-08-26 20:47:07 -0300

    Salvando dados no bancoH2

--- src/main/java/com/icaropaixao/cursomodelagem/CursoModelagemApplication.java
+++ src/main/java/com/icaropaixao/cursomodelagem/CursoModelagemApplication.java
@@ -1,13 +1,39 @@
 package com.icaropaixao.cursomodelagem;
 
+import com.icaropaixao.cursomodelagem.domain.Categoria;
+import com.icaropaixao.cursomodelagem.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoModelagemApplication {
+public class CursoModelagemApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoModelagemApplication.class, args);
     }
+
+    // executa quando roda a aplicação
+    // salvando dados no banco
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null,"Informática");
+        Categoria cat2 = new Categoria(null,"Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+
+    }
+
+
+
+
 
 }
