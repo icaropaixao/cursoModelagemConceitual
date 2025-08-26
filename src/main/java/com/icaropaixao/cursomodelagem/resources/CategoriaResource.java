f7dc41c29b73ea89b018cf35e857b137757379fa
Icaro Reis <icaroreis1982@gmail.com>
2025-08-25 21:52:30 -0300

    end point de find criado para ID

--- src/main/java/com/icaropaixao/cursomodelagem/resources/CategoriaResource.java
+++ src/main/java/com/icaropaixao/cursomodelagem/resources/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.icaropaixao.cursomodelagem.resources;
 
 import com.icaropaixao.cursomodelagem.domain.Categoria;
+import com.icaropaixao.cursomodelagem.repositories.CategoriaRepository;
+import com.icaropaixao.cursomodelagem.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +17,14 @@ import java.util.List;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @GetMapping
-    public List<Categoria> listar() {
+    @Autowired
+    private CategoriaService categoriaService;
 
-        Categoria cat1 = new Categoria(1,"Informatica");
-        Categoria cat2 = new Categoria(2,"Escritorio");
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = categoriaService.find(id);
 
-
-        List<Categoria> lista = new ArrayList<Categoria>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+        return ResponseEntity.ok().body(obj);
     }
 
 
